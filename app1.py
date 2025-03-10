7e054b23e967e4938cf757ea4e6fc62cc5bdccf0
Donal260202 <116863910+Donal260202@users.noreply.github.com>
2025-03-10 16:35:09 +0530

    app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import matplotlib.pyplot as plt
 st.set_page_config(page_title="PMFBY ANALYSIS",layout="wide")
 df=pd.read_excel("PMFBY.xlsx")
 #st.dataframe(df)
